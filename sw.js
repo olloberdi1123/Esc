71effb5e941f5a593decbb94aa482e0b523b5cd1
olloberdi1123 <138999801+olloberdi1123@users.noreply.github.com>
2026-03-08 14:53:54 +0500

    Update CACHE_NAME to version v27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'keeng-saas-cache-v26'; // HAR SAFAR KOD O'ZGARSA, SHU RAQAMNI OSHIRING!
+const CACHE_NAME = 'keeng-saas-cache-v27'; // HAR SAFAR KOD O'ZGARSA, SHU RAQAMNI OSHIRING!
 
 const urlsToCache = [
     './',
